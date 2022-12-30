3f212c304c2d300cea5089893941a313767170b3
Daria Lakman <104722436+dashuleshka@users.noreply.github.com>
2022-12-30 17:35:45 +0300

    Add files via upload

---
+++ Spinlock/Spinlock.cpp
@@ -0,0 +1,21 @@
+﻿// Spinlock.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <atomic>
+#include <thread>
+using namespace std;
+
+class Spinlock {
+    atomic <bool> flag;
+public:
+    Spinlock() : flag(false) {}
+
+    void lock() {
+        while (flag.exchange(true));
+    }
+
+    void unlock() {
+        flag.store(false);
+    }
+};
